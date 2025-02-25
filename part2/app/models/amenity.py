6b66a7ee4ce838f85ec8f99a2e7ee59f29ccd612
Juandicode <juandiegomhs@mail.com>
2025-02-25 14:26:03 -0300

    creating amenities

---
+++ part2/app/models/amenity.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+from base import Base
+
+class Amenity(Base):
+    def __init__(self, name):
+        super().__init__()  # Llama al constructor de Base (que maneja el ID y datetime)
+        self.name = name
+
+    # Propiedad para 'name'
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if len(value) > 50:
+            raise ValueError("The name can be a maximum of 50 characters.")
+        if not value:
+            raise ValueError("the email can't be empty")
+        self._name = value
