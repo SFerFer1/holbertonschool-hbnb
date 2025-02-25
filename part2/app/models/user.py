58d99a101b0a499404c6375fb216ca4b64785cc4
SFerFer1 <10048@holbertonstudents.com>
2025-02-25 13:17:15 -0300

    fixing the folder problems

--- part2/app/models/user.py
+++ part2/app/models/user.py
@@ -1,16 +1,33 @@
 import uuid
 from datetime import datetime
+from base import Base
+import re
+from email_validator import validate_email, EmailNotValidError
 
-class User():
-    def __init__(self, firstName, lastName, Email):
-        self.id = str(uuid.uuid4())
-        self.first_name = firstName
-        self.last_name = lastName
-        self.email = Email
+class User(Base):
+    def __init__(self, first_name:str, last_name:str, email:str):
+        super().__init__()
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
         self.is_admin = False
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
 
-       @User.setter
-       def User(self, firstname, lastname, email, ):
-           
+    # Property to 'first_name'
+    @property
+    def first_name(self):
+        return self._first_name
+
+    @first_name.setter
+    def fist_name(self, value):
+        if len(value) > 50:
+            raise ValueError("first name can not be longer than 50 characters")
+
+    # Propery to 'first_name'
+    @property
+    def last_name(self):
+        return self._last_name
+
+    @last_name.setter
+    def fistName(self, value):
+        if len(value) > 50:
+            raise ValueError("last name can not be longer than 50 characters")
