55f2e2537a0ba4848294b5741dbd617678581048
Rodrigo <rodrigoferrer30@gmail.com>
2025-04-01 15:43:14 -0300

    places

--- part4/app/static/js/scripts.js
+++ part4/app/static/js/scripts.js
@@ -8,10 +8,23 @@ document.addEventListener('DOMContentLoaded', () => {
         const email = LoginForm.querySelector("#email").value;
         const password = LoginForm.querySelector("#password").value;
         await loginUser(email, password);
-    }); // ← Este paréntesis y punto y coma estaban mal colocados.
-  } // ← Cierre correcto del `if` 
+    });
   
-}); // ← Cierre correcto del `DOMContentLoaded`
+    
+  }
+const loginButton= document.getElementsByClassName('login-button');
+
+const newCookie = getCookie("token");
+if (!newCookie) {
+  window.location.href = 'login.html';
+} else {
+  for (let i = 0; i < loginButton.length; i++) {
+    loginButton[i].style.display = 'none'; // Ocultar cada botón
+}
+  
+}
+  
+});
 
 async function loginUser(email, password) {
     const response = await fetch("http://127.0.0.1:5000/api/v1/auth/login", {
@@ -28,5 +41,33 @@ async function loginUser(email, password) {
   } else {
       alert('Login failed: ' + response.statusText);
   }
+  
+}
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) {
+
+    const cookieValue = parts.pop();
+
+    const splitValues = cookieValue.split(';');
+
+    const finalValue = splitValues.shift();
+    return finalValue;
+}
+return null
 }
 
+async function getPlaces() {
+  const places = await fetch("http://127.0.0.1:5000/api/v1/places", {
+      method: 'GET',
+      headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+      },
+  }
+
+)
+alert("aaaaaaaaaaaaaaaaa"); // Mostrar los lugares en un alert
+return places;
+};
